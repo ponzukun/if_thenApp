febaba8b9101a1b8fb30c5c61828448adfd66ef5
ponzukun <p1o1n1z1u@gmail.com>
2019-11-23 21:42:28 +0900

    Add error messages and test on failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      # ↑の戻り値は、許可された属性のみが含まれたparamsのハッシュ (:user属性がない場合はエラー)。
+    end
 end
